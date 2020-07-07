6d8938e01f073aa0184e8e34549d22e6907cef81
Aniruddha Sardar <aniruddha.sardar@gmail.com>
2020-07-07 20:28:39 +0530

    Some ore changes

--- src/main/java/com/guitar/theory/GuitarTheoriesApplication.java
+++ src/main/java/com/guitar/theory/GuitarTheoriesApplication.java
@@ -9,5 +9,7 @@ public class GuitarTheoriesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GuitarTheoriesApplication.class, args);
 	}
+	
+
 
 }
--- src/main/java/com/guitar/theory/service/songs/SongsWithChordsService.java
+++ src/main/java/com/guitar/theory/service/songs/SongsWithChordsService.java
@@ -3,6 +3,8 @@ package com.guitar.theory.service.songs;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +15,11 @@ import com.guitar.theory.domain.SongsBean;
 public class SongsWithChordsService {
 	@Autowired
 	SongsWithChordsDao songsWithChordsDao;
+	
+	private static final Logger logger = LoggerFactory.getLogger(SongsWithChordsService.class);
 
 	public List<SongsBean> getSongsWithChords(SongsBean songsBean) {
+		logger.info("Entering getSongsWithChords(..)");
 		List<SongsBean> listOfSongs = new ArrayList<>();
 
 		/*
